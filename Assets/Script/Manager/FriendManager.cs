a264bfaf608a1ba22e75ce81776dff4b464b66ae
Nattapong Prachan <a@seventop.games>
2020-01-09 16:21:51 +0700

    no message

---
+++ Assets/Script/Manager/FriendManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class FriendManager : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+
+    }
+}
---
+++ Assets/Script/Photon/ReconnectController.cs
@@ -0,0 +1,42 @@
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReconnectController : MonoBehaviourPunCallbacks
+{
+    private void OnApplicationPause(bool isPaused)
+    {
+        if (isPaused)
+        {
+            PhotonNetwork.Disconnect();
+        }
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("OnDisconnected "+cause);
+        PhotonNetwork.LoadLevel((int)SceneIndex.Lobby);
+    }
+    private void OnPlayerDisconnected()
+    {
+        Debug.Log("OnDisconOnPlayerDisconnectednected");
+        PhotonNetwork.LoadLevel((int)SceneIndex.Lobby);
+    }
+    private void Update()
+    {
+        if (PhotonNetwork.NetworkingClient.LoadBalancingPeer.PeerState == PeerStateValue.Disconnected)
+        {
+            if (!PhotonNetwork.ReconnectAndRejoin())
+            {
+                Debug.Log("Failed reconnecting and joining!!", this);
+            }
+            else
+            {
+                Debug.Log("Successful reconnected and joined!", this);
+                Debug.Log("nickname "+PhotonNetwork.LocalPlayer.NickName);
+            }
+        }
+    }
+}
